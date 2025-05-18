c030506bb572397fa24edd7d71e9df0bdcc669e2
Hasan Çağrı Güngör <61599311+hasancagrigungor@users.noreply.github.com>
2025-05-18 10:41:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ c=conn.cursor()
 if len(ara)>2:
     c.execute(f"SELECT * FROM haberler WHERE baslik LIKE '%{ara}%' ORDER BY trend_id DESC LIMIT 99 ")
 
-c.execute("SELECT * FROM haberler ORDER BY trend_id DESC LIMIT 99")
+else:
+    c.execute("SELECT * FROM haberler ORDER BY trend_id DESC LIMIT 99")
+    
 haberler=c.fetchall()
 
 for i in range(0,len(haberler),3):
