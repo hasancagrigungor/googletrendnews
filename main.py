6096d4a845f942721d0f028f6434bf07e5314b35
Hasan Çağrı Güngör <61599311+hasancagrigungor@users.noreply.github.com>
2025-05-18 11:07:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ else:
     
 haberler=c.fetchall()
 
+if len(haberler)==0:
+    st.warning("Herhangi Bir Haber bulunamadı")
+
 for i in range(0,len(haberler),3):
     col1,col2,col3=st.columns(3)
 
